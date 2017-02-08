cc40ec26ac99f90b89e300b09c38d3f594fdbdd2
Tania <tanin_54@hotmail.com>
2017-02-08 15:47:35 -0500

    creacion base datos

---
+++ app/src/main/java/com/tincio/popularmovies/data/model/FavoriteDataBase.java
@@ -0,0 +1,27 @@
+package com.tincio.popularmovies.data.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by tincio on 08/02/17.
+ */
+
+public class FavoriteDataBase extends SQLiteOpenHelper {
+
+
+    public FavoriteDataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
