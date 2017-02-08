b48e44cf898930ea8f2a2ea9c8984ae23bed67a9
Tania <tanin_54@hotmail.com>
2017-02-08 16:48:49 -0500

    creacio content provider

--- app/src/main/java/com/tincio/popularmovies/data/model/FavoriteDataBase.java
+++ app/src/main/java/com/tincio/popularmovies/data/model/FavoriteDataBase.java
@@ -10,18 +10,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class FavoriteDataBase extends SQLiteOpenHelper {
 
-
+    String sqlCreate = "CREATE TABLE Favorite " +
+            "(_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            " nombre TEXT)";
     public FavoriteDataBase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(sqlCreate);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(sqlCreate);
     }
+
+
 }
---
+++ app/src/main/java/com/tincio/popularmovies/data/model/PopularMoviesContentProvider.java
@@ -0,0 +1,141 @@
+package com.tincio.popularmovies.data.model;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.provider.BaseColumns;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by tincio on 08/02/17.
+ */
+
+public class PopularMoviesContentProvider extends ContentProvider {
+    private static final String uri =
+            "content://com.tincio.popularmovies.contentproviders/Favorite";
+
+    public static final Uri CONTENT_URI = Uri.parse(uri);
+
+    /**Para poder acceder a la tabla y/o valores de la tabla*/
+    //UriMatcher
+            //acceso generico a la tabla
+    private static final int FAVORITE = 1;
+    //acceso a una determinada fila
+    private static final int FAVORITE_ID = 2;
+    private static  UriMatcher uriMatcher = null;
+
+    /**Variables para acceder a la base de datos*/
+    //Base de datos
+    private FavoriteDataBase favoriteDataBase;
+    private static final String BD_NOMBRE = "DBFavorite";
+    private static final int BD_VERSION = 1;
+    private static final String TABLE_FAVORITE = "Favorite";
+
+    @Override
+    public boolean onCreate() {
+        favoriteDataBase = new FavoriteDataBase(
+                getContext(), BD_NOMBRE, null, BD_VERSION);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+            //Si es una consulta a un ID concreto construimos el WHERE
+            String where = selection;
+            if(uriMatcher.match(uri) == FAVORITE_ID){
+                where = "_id=" + uri.getLastPathSegment();
+            }
+
+            SQLiteDatabase db = favoriteDataBase.getWritableDatabase();
+
+            Cursor c = db.query(TABLE_FAVORITE, projection, where,
+                    selectionArgs, null, null, sortOrder);
+
+            return c;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        int match = uriMatcher.match(uri);
+
+        switch (match)
+        {
+            case FAVORITE:
+                return "vnd.android.cursor.dir/vnd.tincio.favorite";
+            case FAVORITE_ID:
+                return "vnd.android.cursor.item/vnd.tincio.favorite";
+            default:
+                return null;
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        long regId = 1;
+
+        SQLiteDatabase db = favoriteDataBase.getWritableDatabase();
+
+        regId = db.insert(TABLE_FAVORITE, null, values);
+
+        Uri newUri = ContentUris.withAppendedId(CONTENT_URI, regId);
+
+        return newUri;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        int cont;
+
+        //Si es una consulta a un ID concreto construimos el WHERE
+        String where = selection;
+        if(uriMatcher.match(uri) == FAVORITE_ID){
+            where = "_id=" + uri.getLastPathSegment();
+        }
+
+        SQLiteDatabase db = favoriteDataBase.getWritableDatabase();
+
+        cont = db.delete(TABLE_FAVORITE, where, selectionArgs);
+
+        return cont;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        int cont;
+
+        //Si es una consulta a un ID concreto construimos el WHERE
+        String where = selection;
+        if(uriMatcher.match(uri) == FAVORITE_ID){
+            where = "_id=" + uri.getLastPathSegment();
+        }
+
+        SQLiteDatabase db = favoriteDataBase.getWritableDatabase();
+
+        cont = db.update(TABLE_FAVORITE, values, where, selectionArgs);
+
+        return cont;
+    }
+
+    public static final class Favorite implements BaseColumns
+    {
+        private Favorite() {}
+
+        public static final String COL_NOMBRE = "nombre";
+    }
+
+    /**Urimatcher*/
+    //Inicializamos el UriMatcher
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.tincio.popularmovies.contentproviders", "favorite", FAVORITE);
+        uriMatcher.addURI("com.tincio.popularmovies.contentproviders", "favorite/#", FAVORITE_ID);
+    }
+}
